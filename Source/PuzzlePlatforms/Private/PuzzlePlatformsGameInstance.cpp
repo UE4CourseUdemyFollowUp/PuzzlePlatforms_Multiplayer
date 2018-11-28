927e0e8ebc40465b0df4723f498da2f8459d91d9
Eduard Korogodov <eduard.korogodov@gmail.com>
2018-11-28 21:04:41 +0200

    Create Session

--- Source/PuzzlePlatforms/Private/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/Private/PuzzlePlatformsGameInstance.cpp
@@ -4,12 +4,14 @@
 #include "Engine/Engine.h"
 #include "UObject/ConstructorHelpers.h"
 #include "Blueprint/UserWidget.h"
-#include "OnlineSubsystem.h"
+#include "OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
 
 #include "PlatformTrigger.h"
 #include "MenuSystem/MainMenu.h"
 #include "MenuSystem/MenuWidget.h"
 
+const static FName SESSION_NAME = TEXT("My Session");
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer)
 {
@@ -40,10 +42,12 @@ void UPuzzlePlatformsGameInstance::Init()
 	if (Subsystem)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Found subsystem interface: %s"), *Subsystem->GetSubsystemName().ToString());
-		IOnlineSessionPtr OnlineSession = Subsystem->GetSessionInterface();
+		OnlineSession = Subsystem->GetSessionInterface();
 		if (OnlineSession.IsValid())
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Found system interface"));
+			OnlineSession->OnCreateSessionCompleteDelegates.AddUObject(this, &UPuzzlePlatformsGameInstance::OnCreateSessionComplete);
+			OnlineSession->OnDestroySessionCompleteDelegates.AddUObject(this, &UPuzzlePlatformsGameInstance::OnDestroySessionComplete);			
 		}
 	}
 	else
@@ -83,21 +87,17 @@ void UPuzzlePlatformsGameInstance::LoadIngameMenuWidget()
 
 void UPuzzlePlatformsGameInstance::HostServer()
 {
-	UE_LOG(LogTemp, Warning, TEXT("[%s]"), *FString(__FUNCTION__));
-	UEngine* Engine = GetEngine();
-	if (!ensure(Engine))
-		return;
-
-	Engine->AddOnScreenDebugMessage(0, 5.f, FColor::White, TEXT("Host!"));
-	
-	UWorld* World = GetWorld();
-
-	if (!ensure(World))
-		return;
-
-	UE_LOG(LogTemp, Warning, TEXT("[%s]"), *FString(__FUNCTION__));
-
-	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+	if (OnlineSession.IsValid())
+	{
+		if (OnlineSession->GetNamedSession(SESSION_NAME))
+		{
+			OnlineSession->DestroySession(SESSION_NAME);
+		}
+		else
+		{
+			CreateSession();
+		}
+	}
 }
 
 void UPuzzlePlatformsGameInstance::JoinGame(const FString& Address)
@@ -125,6 +125,50 @@ void UPuzzlePlatformsGameInstance::LoadMainMenu()
 		return;
 
 	PlayerController->ClientTravel("Game/MenuSystem/MainMenu", ETravelType::TRAVEL_Absolute);
+}
+
+void UPuzzlePlatformsGameInstance::OnCreateSessionComplete(FName SessionName, bool Success)
+{
+	UE_LOG(LogTemp, Warning, TEXT("[%s]"), *FString(__FUNCTION__));
+	
+	if (!Success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could Not Create A Session"));
+		return;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Session Created"));
+	
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine))
+		return;
+
+	Engine->AddOnScreenDebugMessage(0, 5.f, FColor::White, TEXT("Host!"));
+
+	UWorld* World = GetWorld();
+
+	if (!ensure(World))
+		return;
+
+	UE_LOG(LogTemp, Warning, TEXT("[%s]"), *FString(__FUNCTION__));
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::OnDestroySessionComplete(FName SessionName, bool Success)
+{
+	if (Success)
+	{
+		CreateSession();
+	}
+}
+
+void UPuzzlePlatformsGameInstance::CreateSession()
+{
+	if (OnlineSession.IsValid())
+	{
+		FOnlineSessionSettings OnlineSessionSettings;
+		OnlineSession->CreateSession(0, SESSION_NAME, OnlineSessionSettings);
+	}
 }
 
 
--- Source/PuzzlePlatforms/Public/PuzzlePlatformsGameInstance.h
+++ Source/PuzzlePlatforms/Public/PuzzlePlatformsGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSubsystem.h"
 #include "MenuSystem/MenuInterface.h"
 #include "PuzzlePlatformsGameInstance.generated.h"
 
@@ -40,4 +41,11 @@ private:
 	TSubclassOf<class UMenuWidget> IngameMenuClass;
 	
 	class UMainMenu* MainMenu;
+
+	IOnlineSessionPtr OnlineSession;
+
+	void OnCreateSessionComplete(FName SessionName, bool Success);
+	void OnDestroySessionComplete(FName SessionName, bool Success);
+
+	void CreateSession();
 };
